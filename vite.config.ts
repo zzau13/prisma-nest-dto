b8e904a4d24fee1d9f9d60e1db01b02bf6d8fb97
Juan Aguilar Santillana <mhpoin@gmail.com>
2023-06-15 20:00:38 +0200

    chore: exclude coverage of get config file, index and file generator

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ['json', 'text-summary'],
+      exclude: ['src/config.ts', 'src/index.ts', 'src/generate.ts', 'test/*'],
     },
   },
 });
